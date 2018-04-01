1d2976da4da8cbb5a7e2a4d5353790cf4b1d903d
Scott Wang <scott@malloc.sh>
2018-03-31 17:42:39 -0700

    adding testing cases

---
+++ src/MemCache.php
@@ -0,0 +1,221 @@
+<?php
+namespace RW;
+
+use Psr\SimpleCache\CacheInterface;
+
+/**
+ * Class MemCache
+ * @package RW
+ */
+class MemCache implements CacheInterface
+{
+    private $data = [];
+
+
+    /**
+     * Fetches a value from the cache.
+     *
+     * @param string $key The unique key of this item in the cache.
+     * @param mixed $default Default value to return if the key does not exist.
+     *
+     * @return mixed The value of the item from the cache, or $default in case of cache miss.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if the $key string is not a legal value.
+     */
+    public function get($key, $default = null)
+    {
+        if (!$this->isValidKey($key)) {
+            throw new SimpleCacheException("Invalid key. Only [a-zA-Z0-9_-] allowed.");
+        }
+        
+        $now = time();
+
+        if (isset($this->data[$key]) && isset($this->data[$key]['expiry']) && ($this->data[$key]['expiry'] === 0 || $this->data[$key]['expiry'] >= $now)) {
+            return $this->data[$key]['value'];
+        } else if (isset($this->data[$key]) && isset($this->data[$key]['expiry']) && ($this->data[$key]['expiry'] < $now)) {
+            unset($this->data[$key]);
+        }
+
+        return $default;
+    }
+
+    /**
+     * Persists data in the cache, uniquely referenced by a key with an optional expiration TTL time.
+     *
+     * @param string $key The key of the item to store.
+     * @param mixed $value The value of the item to store, must be serializable.
+     * @param null|int|\DateInterval $ttl Optional. The TTL value of this item. If no value is sent and
+     *                                      the driver supports TTL then the library may set a default value
+     *                                      for it or let the driver take care of that.
+     *
+     * @return bool True on success and false on failure.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if the $key string is not a legal value.
+     */
+    public function set($key, $value, $ttl = null)
+    {
+        if (!$this->isValidKey($key)) {
+            throw new SimpleCacheException("Invalid key. Only [a-zA-Z0-9_-] allowed.");
+        }
+
+        if ($this->isValidTTL($ttl)) {
+            throw new SimpleCacheException("TTL must only be integer greater than 0 or null.");
+        }
+
+        $this->data[$key]['value'] = $value;
+        if ($ttl <= 0) {
+            $this->data[$key]['expiry'] = 0;
+        } else {
+            $this->data[$key]['expiry'] = time() + $ttl;
+        }
+
+        return true;
+    }
+
+    /**
+     * Delete an item from the cache by its unique key.
+     *
+     * @param string $key The unique cache key of the item to delete.
+     *
+     * @return bool True if the item was successfully removed. False if there was an error.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if the $key string is not a legal value.
+     */
+    public function delete($key)
+    {
+        if (!$this->isValidKey($key)) {
+            throw new SimpleCacheException("Invalid key. Only [a-zA-Z0-9_-] allowed.");
+        }
+        unset($this->data[$key]);
+        return true;
+    }
+
+    /**
+     * Wipes clean the entire cache's keys.
+     *
+     * @return bool True on success and false on failure.
+     */
+    public function clear()
+    {
+        $this->data = [];
+        return true;
+    }
+
+    /**
+     * Obtains multiple cache items by their unique keys.
+     *
+     * @param iterable $keys A list of keys that can obtained in a single operation.
+     * @param mixed $default Default value to return for keys that do not exist.
+     *
+     * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if $keys is neither an array nor a Traversable,
+     *   or if any of the $keys are not a legal value.
+     */
+    public function getMultiple($keys, $default = null)
+    {
+        if (!is_array($keys) || empty($keys)) {
+            throw new SimpleCacheException("Keys must be array and cannot be empty.");
+        }
+
+        $results = [];
+        foreach ($keys as $key) {
+            $results[$key] = $this->get($key, $default);
+        }
+        return $results;
+    }
+
+    /**
+     * Persists a set of key => value pairs in the cache, with an optional TTL.
+     *
+     * @param iterable $values A list of key => value pairs for a multiple-set operation.
+     * @param null|int|\DateInterval $ttl Optional. The TTL value of this item. If no value is sent and
+     *                                       the driver supports TTL then the library may set a default value
+     *                                       for it or let the driver take care of that.
+     *
+     * @return bool True on success and false on failure.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if $values is neither an array nor a Traversable,
+     *   or if any of the $values are not a legal value.
+     */
+    public function setMultiple($values, $ttl = null)
+    {
+        foreach ($values as $k =>$v) {
+            $this->set($k, $v, $ttl);
+        }
+
+        return true;
+    }
+
+    /**
+     * Deletes multiple cache items in a single operation.
+     *
+     * @param iterable $keys A list of string-based keys to be deleted.
+     *
+     * @return bool True if the items were successfully removed. False if there was an error.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if $keys is neither an array nor a Traversable,
+     *   or if any of the $keys are not a legal value.
+     */
+    public function deleteMultiple($keys)
+    {
+        foreach ($keys as $k) {
+            $this->delete($k);
+        }
+        return true;
+    }
+
+    /**
+     * Determines whether an item is present in the cache.
+     *
+     * NOTE: It is recommended that has() is only to be used for cache warming type purposes
+     * and not to be used within your live applications operations for get/set, as this method
+     * is subject to a race condition where your has() will return true and immediately after,
+     * another script can remove it making the state of your app out of date.
+     *
+     * @param string $key The cache item key.
+     *
+     * @return bool
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *   MUST be thrown if the $key string is not a legal value.
+     */
+    public function has($key)
+    {
+        if (!$this->isValidKey($key)) {
+            throw new SimpleCacheException("Invalid key. Only [a-zA-Z0-9_-] allowed.");
+        }
+
+        $now = time();
+        return (isset($this->data[$key])
+                    && isset($this->data[$key]['expiry'])
+                        && ($this->data[$key]['expiry'] === 0 || $this->data[$key]['expiry'] >= $now));
+    }
+
+    /**
+     * Check if a given key is valid
+     *
+     * @param $key
+     * @return bool
+     */
+    private function isValidKey($key)
+    {
+        return (!empty($key) && ctype_alnum(str_replace(['-', '_'], '', $key)));
+    }
+
+    /**
+     * Check if a given ttl is valid
+     * @param $ttl
+     * @return bool
+     */
+    private function isValidTTL($ttl)
+    {
+        return ($ttl !== null && (!is_int($ttl) || $ttl <= 0));
+    }
+}
